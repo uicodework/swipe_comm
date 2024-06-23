9a148c5fcfa8326bc937ad7cd89b0388a890d4ef
Halmo <halmavanszki.janos@gmail.com>
2024-06-23 21:55:41 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,289 @@
+import 'dotenv/config';
+import { Client, TaskCommand, TaskEvent, Environment, OrderEventData, UniversalMenuItem } from 'swipelime-client-node';
+import { NativeTable, ServiceHandler, TablesEventData } from 'swipelime-client-node/dist/ServiceHandler';
+var bodyParser = require('body-parser')
+const mysql = require('mysql');
+
+var express = require('express');
+
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+import api, { SqlExec, SqlExecute, SqlInsert, SqlOneValue, SqlOneValueDbl, eloZaras, newOrder } from "./routes/api";
+
+process.argv.forEach((val, index) => {
+    if (val === "--autostart")
+        startService();
+    //console.log(`${index}: ${val}`);
+});
+
+app.use('/api', api);
+
+app.get('/', (req: any, res: any) => {
+    //console.log(username);
+    res.send("OK");
+});
+
+app.get('/verzio', (req: any, res: any) => {
+    res.send("verzió:" + process.env['VERZIO']);
+});
+
+app.get('/start', (req: any, res: any) => {
+    startService().then((ret) => res.json('status:' + ret));
+
+});
+
+app.get('/isready', (req: any, res: any) => {
+    try {
+        pserviceHandler.isReady().then((ret) => { res.json('status:' + ret) });
+    }
+    catch (error) {
+        res.send("HIBA");
+        console.error(error);
+    }
+
+});
+
+let pserviceHandler: ServiceHandler;
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('App listening on port: ' + port);
+    console.log('App version: ' + process.env['VERZIO']);
+});
+
+export function getserviceHandler() {
+    return pserviceHandler;
+}
+
+export function startService() {
+    return new Promise((resolve, reject) => {
+
+        const username = process.env['SWIPELIME_USERNAME'];
+        const password = process.env['SWIPELIME_PASSWORD'];
+        const tenantid = process.env['SWIPELIME_TENANT'] as string;
+        const environment = process.env['SWIPELIME_ENVIRONMENT'] as Environment | undefined;
+
+        if (username === undefined || password === undefined) throw new Error('SWIPELIME_USERNAME or SWIPELIME_PASSWORD environment variable not defined');
+
+        const client = new Client({ username, password }, { environment });
+
+        // Register a service handler for one of the available tenants
+        (async () => {
+            client.emitter.on('connected', async () => {
+                console.log('swipelime client connected');
+                console.log('client version: '+ client.clientVersion);
+            });
+
+            client.emitter.on('login', async (user) => {
+                console.log('logged in user ID', user.id);
+                const availableTenants = await client.getAvailableTenantIds();
+                console.log('available tenants', availableTenants);
+            });
+
+            client.emitter.on('error', (e) => {
+                console.error('error', e.message);
+            });
+
+            const serviceHandler = await client.addServiceHandler({ tenantId: tenantid });
+            pserviceHandler = serviceHandler;
+
+            serviceHandler.emitter.on('newTasks', (tasks) => {
+                tasks.forEach(async (task) => {
+                    if (task instanceof TaskEvent) {
+                        console.log(task.data.eventType, JSON.stringify(task.data.eventData, null, 2));
+
+                        if (task.data.eventType === "order-items-confirmed") {
+                            newOrder(task.data.eventData as OrderEventData);
+                        }
+                        else if (task.data.eventType === "payment-requested") {
+                            if (task.data.eventData.tableData.externalId !== undefined)
+                                eloZaras(task.data.eventData.tableData.externalId);
+                        }
+                        else if (task.data.eventType === "universal-menu-elements-added") {
+                            const cikkek = task.data.eventData as UniversalMenuItem[];
+                            cikkek.forEach(c => {
+                                newCikk(c).then((ret) => {
+                                    c.data.externalId = String(ret);
+                                    console.log("upsertUniversalMenuItems");
+                                    console.log(JSON.stringify(c.data, null, 2));
+                                    serviceHandler.upsertUniversalMenuItems([c.data]).then((r) => { console.log(r) }).catch((e) => { console.error(e) });
+                                    etlapTetelInsert(ret as number);
+                                });
+
+                            });
+                        } 
+                        else if (task.data.eventType === "universal-menu-elements-updated") {
+                            const cikkek = task.data.eventData as UniversalMenuItem[];
+                            cikkek.forEach(c => {
+                                if (c.type === "item") { 
+                                    if (c.data.externalId != undefined) {
+                                        updateCikk(c);
+                                    }
+                                }
+                            });
+                        
+                        }
+                        else if (task.data.eventType === "tables-added") {
+                            console.log("új asztal");
+                            const asztalok = task.data.eventData as NativeTable[];
+                            asztalok.forEach( asztal => {
+                                ujasztal(asztal.id, asztal.label.hu, asztal.externalId);
+                            });
+                            //let asztal = task.data.eventData[0];
+                        }
+                        await task.confirm();
+
+                    }
+                    else if (task instanceof TaskCommand) {
+                        console.log(task.data.commandType, JSON.stringify(task.data, null, 2));
+
+                        await serviceHandler.confirmTestCommand(task);
+                    }
+                });
+            });
+        })().then(() => {
+            resolve("OK");
+            console.log("elindult")
+        });
+    });
+
+    function successCallback(result: any) {
+        SqlOneValue('kod', 'cikkcsoportok', ' nev="SWIPLIME"').then((ret) => {
+
+        }
+
+        )
+    }
+
+    async function getCsoportkod(csopkod: string) {
+        return new Promise((resolve, reject) => {
+            if (csopkod.length == 0) {
+                SqlExec("insert into cikkcsoportok (nev, focsoport, ikontipus) values('SWIPELIME',0,6)").then(() => {
+                    SqlOneValue('kod', 'cikkcsoportok', " nev='SWIPELIME'")
+                        .then((ret) => { resolve(ret) })
+                })
+            }
+            else {
+                resolve(csopkod);
+            }
+        })
+    }
+
+    async function insertCikk(csopkod: string, c: UniversalMenuItem) {
+        return new Promise((resolve, reject) => {
+            let nev = c.data.label.hu?.substring(0,119);
+            let rovidnev = nev?.substring(0,24);
+            let brutto = c.data.price;
+            let netto = brutto / 1.27;
+            let afakulcs = 21.26;
+            let kulsoazonosito = c.data.id;
+            let me = "DB";
+            let afakod = 6;
+
+            (async () => {
+                afakod = Number(await SqlOneValue("min(kod) as kod", "afakulcs", "ertek = 27"));
+            })().then(() => {
+
+                let sql = `insert into cikk (nev, rovidnev, csoportkod, brutto, nettoar, afakulcs, kulsoazonosito, me, afakod) 
+                        values (${mysql.escape(nev)}, ${mysql.escape(rovidnev)}, ${csopkod}, ${brutto}, ${netto}, ${afakulcs}, ${mysql.escape(kulsoazonosito)}, ${mysql.escape(me)}, ${afakod})`;
+
+                SqlInsert(sql).then((ret) => {
+                        //let val = JSON.parse(JSON.stringify(ret));
+                        //let ujid = val[0].res;
+                        console.log("új id: " + ret);
+                        resolve(ret);
+                });
+            });
+        })
+    }
+
+    async function newCikk(c: UniversalMenuItem) {
+        return new Promise((resolve, reject) => {
+
+            SqlOneValue("kod", "cikkcsoportok", "nev='SWIPELIME'")
+                .then((ret) => getCsoportkod(String(ret))
+                    .then((kod) => insertCikk(String(kod), c))
+                    .then((cikkid) => { resolve(cikkid) })
+                )
+        })
+
+    }
+
+    function ujasztal(id: string, name: string | undefined, extid: string | undefined) {
+        if ((name == undefined) && (extid == undefined)) {
+            return;
+        }
+
+        //helyiség ellenőrzés
+        SqlExecute("INSERT IGNORE INTO helyisegek (kod, nev) VALUES ('SWIPELIME', 'SWIPELIME')");
+
+        if (extid == undefined) {
+            SqlExecute("INSERT IGNORE INTO asztalok (asztalszam, helyisegkod, statusz, balx, baly, jobbx, jobby, torolt, alak) " +
+                " VALUES ('" + name + "', 'SWIPELIME', 'zart', 0, 0, 20, 20, 'N', 1)");
+
+            //visszaküldjük az azonosítót, hogy ki legyen töltve a külső azonosító
+            let table: any;
+            table = {
+                id: id,
+                externalId: name,
+                label: { hu: name }
+            };
+            console.log(table);
+
+            getserviceHandler().upsertTables([table]).then((ret) => {
+                console.log("upsertTables");
+                console.log(ret);
+                
+            });
+
+        } else {
+            SqlExecute("INSERT IGNORE INTO asztalok (asztalszam, helyisegkod, statusz, balx, baly, jobbx, jobby, torolt, alak) " +
+                " VALUES ('" + extid + "', 'SWIPELIME', 'zart', 0, 0, 20, 20, 'N', 1)");
+
+        }
+
+
+
+    }
+
+    function updateCikk(cikk: UniversalMenuItem){
+        
+        let tiltott = "N";
+        if (!cikk.data.enabled){
+            tiltott = "I";
+        }
+
+        let sql = "update cikk set nev = " + mysql.escape(cikk.data.label.hu) +
+                  " , tiltott = " + mysql.escape(tiltott) +  
+                  " where kod = " + cikk.data.externalId;
+
+        SqlExecute( sql );
+        
+        SqlOneValueDbl("afakulcs", "cikk", "kod = " + cikk.data.externalId).then( (afakulcs) => {
+            let brutto = cikk.data.price;
+            let netto = (100.0-afakulcs)/100.0 * brutto;
+            sql = `update cikk set brutto = ${brutto}, nettoar =round( ${netto},2) where kod = ${cikk.data.externalId}`;
+            SqlExecute(sql);
+            sql = `update etlaptetelek set brutto = ${brutto}, nettoar =round( ${netto},2) where cikkkod = ${cikk.data.externalId}`;
+            SqlExecute(sql);
+
+        });
+
+    }
+
+    function etlapTetelInsert(cikkkod: number){
+        let sql = `INSERT INTO etlaptetelek (etlapkod, cikkkod, brutto, nettoar, aktiv) ` +
+        ` SELECT e.kod, c.kod, c.brutto, c.nettoar, 'I' ` +
+        ` FROM cikk c, etlap e ` +
+        ` WHERE c.kod = ${cikkkod}`; 
+        SqlExecute(sql);
+    }
+
+
+}
---
+++ routes/api.ts
@@ -0,0 +1,332 @@
+import { OrderEventData, TaskCommandData } from "swipelime-client-node";
+import { getserviceHandler, startService } from "..";
+
+var express = require('express');
+const mysql = require('mysql');
+
+let pool =
+    mysql.createPool({
+        connectionLimit: 100, //important
+        host: process.env['MYSQL_HOST'],
+        user: process.env['MYSQL_USER'],
+        password: process.env['MYSQL_PASSW'],
+        database: process.env['MYSQL_DB'],
+        debug: false
+    });
+
+const router = express();
+
+router.get('/', (req: any, res: any) => {
+    console.log(process.env['MYSQL_HOST']);
+    getMySQLInfo().then((ret) => {
+        res.json(ret);
+    });
+    //res.send(process.env['MYSQL_DB']);
+});
+
+router.post('/', (req: any, res: any) => {
+    const { command, data } = req.body;
+    console.log(process.env['MYSQL_HOST']);
+    console.log(command);
+    console.log(data);
+    //console.log(req.body);
+
+    if (command === 'finishTable') {
+        const { asztal } = data;
+        console.log("Zárni: " + asztal);
+        getserviceHandler().finishTable({ externalId: asztal }).then((res) => {
+            console.log("Finish table");
+            console.log(res);
+        }).catch((err) => {
+            console.log("Finish table HIBA");
+            console.log(err);
+        });
+        res.json({ status: 'SUCCESS' });
+
+    } else if (command === 'upsertUniversalMenuItems') {
+        getserviceHandler().upsertUniversalMenuItems(data).then((res) => {
+            console.log("upsertUniversalMenuItems");
+            console.log(res);
+        });
+        res.json({ status: 'SUCCESS' });
+    }
+    else if (command === 'isready') {
+        try {
+            getserviceHandler().isReady().then((ret) => {
+                res.json({ status: 'OK' });
+            });
+        }
+        catch (error) {
+            res.json({ status: 'ERROR' });
+            console.error(error);
+        }
+        //  getserviceHandler().getUniversalMenuCategories().then((ret) => {
+        //      console.log(ret);
+        //  });
+    }
+    else if (command === 'start') {
+        //console.log(data.MYSQL_DB);
+        process.env['SWIPELIME_USERNAME'] = data.SWIPELIME_USERNAME;
+        process.env['SWIPELIME_PASSWORD'] = data.SWIPELIME_PASSWORD;
+        process.env['SWIPELIME_TENANT'] = data.SWIPELIME_TENANT;
+        process.env['SWIPELIME_ENVIRONMENT'] = data.SWIPELIME_ENVIRONMENT;
+        process.env['MYSQL_DB'] = data.MYSQL_DB;
+        process.env['MYSQL_USER'] = data.MYSQL_USER;
+        process.env['MYSQL_PASSW'] = data.MYSQL_PASSW;
+        process.env['MYSQL_HOST'] = data.MYSQL_HOST;
+
+        pool = mysql.createPool({
+            connectionLimit: 100, //important
+            host: process.env['MYSQL_HOST'],
+            user: process.env['MYSQL_USER'],
+            password: process.env['MYSQL_PASSW'],
+            database: process.env['MYSQL_DB'],
+            debug: false
+        });
+        startService().then((ret) => res.send(ret)).catch((ret) => { console.log(ret) });
+
+        //res.send("OK");
+    }
+    else if (command === 'upsertTables') {
+        try {
+            getserviceHandler().upsertTables(data).then((ret) => {
+                console.log("upsertTables");
+                console.log(ret);
+                res.send(ret);
+            });
+        }
+        catch (error) {
+            console.log(error);
+            res.send("Hiba");
+        }
+    }
+    else if (command === 'addCustomOrderItem') {
+        getserviceHandler().addCustomOrderItem({ externalId: data.asztal },
+            { label: data.label, quantity: data.quantity, price: data.price }).then((ret) => {
+                console.log("addCustomOrderItem");
+                console.log(ret);
+                res.send(ret);
+            });
+    }
+
+
+});
+
+router.get('/addCustomOrderItem', (req: any, res: any) => {
+
+    getserviceHandler().addCustomOrderItem({ externalId: "01" }, { label: { hu: "Cola" }, quantity: 1, price: 1000 }).then((res) => {
+        console.log("addCustomOrderItem");
+        console.log(res);
+    });
+    res.send("OK");
+});
+
+router.get('/asztalzar', (req: any, res: any) => {
+    getserviceHandler().finishTable({ externalId: "01" }).then((res) => {
+        console.log("Finish table");
+        console.log(res);
+    });
+    res.send("OK");
+});
+
+async function getMySQLInfo() {
+
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err: any, connection: any) => {
+            if (err) throw err;
+
+            connection.query("show variables where value<>''", (err: any, rows: any) => {
+                connection.release(); // return the connection to pool
+                if (err) {
+                    reject(err);
+                } else {
+                    // console.log(rows);
+                    resolve(rows)
+                }
+            });
+        });
+
+    })
+
+}
+
+
+
+export function SqlExecute(sql: string) {
+    console.log('SqlExecute: ' + sql);
+
+    pool.getConnection((err: any, connection: any) => {
+        if (err) throw err;
+
+        connection.query(sql, (err: any, rows: any) => {
+            connection.release(); // return the connection to pool
+            console.log(`Rows: ${rows.affectedRows}`);
+        });
+    });
+
+}
+
+export async function SqlExec(sql: string) {
+    return new Promise((resolve, reject) => {
+        console.log('SqlExec: ' + sql);
+
+        pool.getConnection((err: any, connection: any) => {
+            if (err) throw err;
+
+            connection.query(sql, (err: any, rows: any) => {
+                connection.release(); // return the connection to pool
+                if (err) {
+                    reject(err);
+                } else {
+                    // console.log(rows);
+                    console.log(`Rows: ${rows.affectedRows}`);
+                    resolve(rows);
+                }
+            });
+        });
+
+    })
+
+}
+
+export async function SqlInsert(sql: string) {
+    return new Promise<number>((resolve, reject) => {
+        console.log('SqlInsert: ' + sql);
+        pool.getConnection((err: any, connection: any) => {
+            if (err) throw err;
+
+            connection.query(sql, (err: any, rows: any) => {
+
+                connection.release(); // return the connection to pool
+                if (err) {
+                    reject(err);
+                } else {
+                    // console.log(rows);
+                    console.log(`Inserted ID: ${rows.insertId}`);
+                    resolve(rows.insertId);
+                }
+            });
+        });
+
+    })
+
+}
+
+export function SqlOneValue(field: string, table: string, where: string) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err: any, connection: any) => {
+            if (err) throw err;
+            let sql = `select ${field}`;
+
+            if (table.length > 0) {
+                sql += ` from ${table}`;
+            }
+            if (where.length > 0) {
+                sql += " where " + where;
+            }
+            console.log("SqlOneValue: " + sql);
+            connection.query(sql, (err: any, rows: any, fields: any) => {
+
+                connection.release(); // return the connection to pool
+                if (err) {
+                    reject(err);
+                } else {
+                    //
+                    console.log(rows);
+                    // console.log(fields);
+                    // console.log(fields[0].name);
+                    let ret = '';
+                    if (rows.length > 0) {
+                        ret = rows[0][fields[0].name]
+                    }
+                    resolve(ret);
+                }
+            });
+        });
+
+    })
+
+}
+
+export function SqlOneValueDbl(field: string, table: string, where: string) {
+    return new Promise<number>((resolve, reject) => {
+        pool.getConnection((err: any, connection: any) => {
+            if (err) throw err;
+            let sql = `select ${field}`;
+
+            if (table.length > 0) {
+                sql += ` from ${table}`;
+            }
+            if (where.length > 0) {
+                sql += " where " + where;
+            }
+            console.log("SqlOneValueDbl: " + sql);
+            connection.query(sql, (err: any, rows: any, fields: any) => {
+
+                connection.release(); // return the connection to pool
+                if (err) {
+                    reject(err);
+                } else {
+                    //
+                    console.log(rows);
+                    // console.log(fields);
+                    // console.log(fields[0].name);
+                    let ret = 0.0;
+                    if (rows.length > 0) {
+                        ret = rows[0][fields[0].name]
+                    }
+                    resolve(ret);
+                }
+            });
+        });
+
+    })
+}
+
+
+export async function SqlOneValueStr__(sql: string) {
+    pool.getConnection((err: any, connection: any) => {
+        if (err) throw err;
+
+        connection.promise().query(sql)
+            .then((rows: any, fields: any) => {
+                const ret = rows[0][fields];
+                return ret;
+            })
+            .catch(console.log)
+    });
+}
+
+export function newOrder(order: OrderEventData) {
+    console.log("Új rendelés");
+    console.log("Asztal: " + order.tableData.externalId);
+
+    let asztal = order.tableData.externalId;
+    if (asztal != undefined) {
+        SqlExecute("update asztalok set statusz='nyitott', nyito='SWIPE' " +
+            " where asztalszam='" + asztal + "'" +
+            "  and statusz='zart'");
+
+        order.orderItems.forEach(r => {
+            let cikkkod = r.menuItemData.externalId;
+            let mennyiseg = r.quantity;
+            if (cikkkod != undefined) {
+                let sql = `insert into rendelesek (cikkkod,mennyiseg,kezelokod,ar,brutto,asztalkod,szek,datum,kedvezmenyezheto) ` +
+                    ` values( ${cikkkod}, ${mennyiseg}, "SWIPE", 0, 0, "${asztal}", 1, "", "I") `;
+
+                SqlExecute(sql);
+            } else {
+                console.log("Nincs GTSG azonosító megadva a cikknél!");
+            }
+
+        });
+    } else {
+        console.log("Nincs GTSG azonosító megadva az asztalnál!");
+    }
+}
+
+export function eloZaras(asztal: string) {
+    SqlExecute(`update asztalok set pda=1 where asztalszam="${asztal}"`);
+}
+
+export default router;
